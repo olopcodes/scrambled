26fffa6b808eea7a1c99aef9e9d380a7ebcca6c8
olopcodes <olopcodes@gmail.com>
2024-07-14 16:59:31 -0400

    added elements for javascript and displayed the main title on larger screens

---
+++ app.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll(".game__buttons button");
+const gameWordDisplay = document.querySelector(".game__word");
+const gameTriesSpanText = document.querySelector(".game__stats__text span");
+const gameTriesCircles = document.querySelectorAll(".game__tries__circle");
+const gameMistakesText = document.querySelector(".game__mistakes__display p");
+const gameFieldsTextEl = document.querySelector(".game__text__fields");
