50eab5fd73776a60d9bce74f7f2589f0d87ce771
olopcodes <olopcodes@gmail.com>
2024-07-14 22:12:55 -0400

    added alert text, and some js todos

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
+const words = [
+  "flower",
+  "hijack",
+  "squawk",
+  "quirky",
+  "frenzy",
+  "object",
+  "chazan",
+  "schnoz",
+  "defuse",
+  "claque",
+  "judice",
+  "knubby",
+  "quince",
+  "subfix",
+  "whacky",
+  "zenith",
+  "cajole",
+  "enzym",
+  "bijou",
+  "squab",
+  "vizor",
+  "quips",
+  "maize",
+  "pygmy",
+  "amuck",
+  "axiom",
+  "hokum",
+  "smock",
+  "skimp",
+  "balmy",
+  "bokeh",
+];
 const buttons = document.querySelectorAll(".game__buttons button");
 const gameWordDisplay = document.querySelector(".game__word");
 const gameTriesSpanText = document.querySelector(".game__stats__text span");
 const gameTriesCircles = document.querySelectorAll(".game__tries__circle");
 const gameMistakesText = document.querySelector(".game__mistakes__display p");
 const gameFieldsTextEl = document.querySelector(".game__text__fields");
+
+// number of tries should be the amount of letters in the chosen word - 1
+
+// hide game alert when user submits word for the first time
+
+// scramble word
+
+// show selected word to user
