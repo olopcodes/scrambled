b0e6b3c48dbfb3979c1e7ae005b91ed8a5aba9d4
olopcodes <olopcodes@gmail.com>
2024-07-15 22:15:02 -0400

    added js for getting and displaying word, showing squaress, minor changes to css

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let word;
+let jumbleWord;
+let numberOfTries;
 const words = [
   "flower",
   "hijack",
@@ -5,6 +8,14 @@ const words = [
   "quirky",
   "frenzy",
   "object",
+  "bistro",
+  "circle",
+  "eskimo",
+  "hyrax",
+  "numbat",
+  "reign",
+  "region",
+  "edible",
   "chazan",
   "schnoz",
   "defuse",
@@ -31,17 +42,96 @@ const words = [
   "balmy",
   "bokeh",
 ];
+
 const buttons = document.querySelectorAll(".game__buttons button");
 const gameWordDisplay = document.querySelector(".game__word");
 const gameTriesSpanText = document.querySelector(".game__stats__text span");
-const gameTriesCircles = document.querySelectorAll(".game__tries__circle");
+const gameTriesCircles = document.querySelector(".game__tries__circles");
 const gameMistakesText = document.querySelector(".game__mistakes__display p");
 const gameFieldsTextEl = document.querySelector(".game__text__fields");
 
-// number of tries should be the amount of letters in the chosen word - 1
+// get random word
+function getRandomWord() {
+  const word = words[Math.floor(Math.random() * words.length)];
+  return word;
+}
 
-// hide game alert when user submits word for the first time
+function getJumbleWord(randWord) {
+  // reverse the string and convert to array
+  const wordArr = randWord.split("");
 
-// scramble word
+  // slice the array it two and reverse again
+  // making the slice of the words be more random
+  let slice1;
+  let slice2;
 
-// show selected word to user
+  if (Math.random() > 0.5) {
+    slice1 = wordArr.slice(0, 2).reverse();
+    slice2 = wordArr.slice(2).reverse();
+  } else {
+    slice1 = wordArr.slice(0, 3).reverse();
+    slice2 = wordArr.slice(3).reverse();
+  }
+
+  // join the arrays together and turn to string
+  return slice1.concat(slice2).join("");
+}
+
+// make sure next word is not if dupilcate
+function checkWordChosen(newWord) {
+  if (newWord === word) getRandomWord();
+}
+
+// display word to user
+function displayJumbledWord(newJumbledWord) {
+  gameWordDisplay.textContent = newJumbledWord;
+}
+
+// display number of tries
+function displayNumberOfTries(randWord) {
+  gameTriesSpanText.textContent = `(0/${randWord.length - 1})`;
+}
+
+// create circles based on the length of word, add it to the dom
+function displayNumberOfTriesCircles(randWord) {
+  for (let i = 0; i < randWord.length - 1; i++) {
+    const div = document.createElement("div");
+    div.classList.add("game__tries__circle");
+    gameTriesCircles.append(div);
+  }
+}
+
+// show empty square boxes based on word
+function displayBasedOnWord(randWord) {
+  for (let i = 0; i < randWord.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("game__text__field", "flex--center", "highlight");
+    gameFieldsTextEl.append(div);
+  }
+}
+
+function updateNumberOfColumnsForText(randWord) {
+  if (
+    (randWord === 6 && gameFieldsTextEl.classList.contains("six")) ||
+    (randWord === 5 && gameFieldsTextEl.classList.contains("five"))
+  )
+    return;
+
+  if (randWord.length === 6) {
+    gameFieldsTextEl.classList.remove("five");
+    gameFieldsTextEl.classList.add("six");
+  } else {
+    gameFieldsTextEl.classList.add("five");
+    gameFieldsTextEl.classList.remove("six");
+  }
+}
+
+(function () {
+  word = getRandomWord();
+  jumbleWord = getJumbleWord(word);
+  displayJumbledWord(jumbleWord);
+  displayNumberOfTries(word);
+  displayNumberOfTriesCircles(word);
+  displayBasedOnWord(word);
+  updateNumberOfColumnsForText(word);
+})();
